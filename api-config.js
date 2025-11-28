e0956ababdd5946d78d8880124feba2b36b64f48
kramplast-code <kramplast@gmail.com>
2025-11-29 00:46:52 +0300

    Implement api function for API requests
    Added api function to handle API requests.

--- api-config.js
+++ api-config.js
@@ -1,1 +1,11 @@
 const API_URL = "https://script.google.com/macros/s/AKfycby162gWeCtcGIdE83RH7_m0Ct4qDPOKtmfWShoCV3vQ8tCzprl8Xbq8uWImiqbKPg4n/exec";
+
+export async function api(method, payload = {}) {
+    const res = await fetch(API_URL, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ method, ...payload })
+    });
+
+    return await res.json();
+}
